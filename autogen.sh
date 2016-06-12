022646091029e41e79f673fa5145d2f91053d0f7
Brent Cook <bcook@rapid7.com>
2016-06-11 21:07:35 -0500

    grab newer versions of config.*

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 autoreconf --install --force
+curl 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' > config.guess
+curl 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD' > config.sub
